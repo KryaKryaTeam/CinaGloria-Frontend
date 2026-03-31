3a2a77e7969d9528cce078bf23732af80a403f11
aanddark <andriyworkkovalenko@gmail.com>
2026-03-31 22:13:34 +0300

    feat: new notifiaction widget

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,24 +1,21 @@
 "use client";
-import container from "@/core/Container";
+
 import NotificationStatus from "@/core/domain/entity/NotificationType.enum";
-import Email from "@/core/domain/value-object/Email";
-import Password from "@/core/domain/value-object/Password";
-import JWTChangeRequest from "@/core/network/requests/JWTRequest";
-import { WsSocket } from "@/core/network/socket/initSocket";
-import ReadedRequest from "@/core/requests/network/ReadedRequest.request";
-import WaveBackground from "@/ui/backgrounds/WaveBackground";
-import { Button } from "@/ui/button";
-import { NotificationWidget } from "@/ui/component/NotificationWidget";
-import SecondDataForm from "@/ui/widgets/auth/SecondDataForm";
+import NotificationWidget from "@/ui/widgets/NotificationWidget";
 
 export default function TestPage() {
-  const service = container.get(ReadedRequest)
-  const aboba = async () => {
-    await service.execute()
-  }
   return (
     <>
-      <NotificationWidget notification={aboba} />
+      <NotificationWidget
+        id="1"
+        status={NotificationStatus.readed}
+        title="Test Notification"
+        from="test@example.com"
+        to="user@example.com"
+        content="This is a test notification."
+        targets={[]}
+        createdAt={new Date()}
+      />
     </>
-  )
+  );
 }
